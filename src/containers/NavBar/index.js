659093b528947d8f78b93fc19294f8429764e98c
srikanth.soparla <srikanth.soparla@cosmiclabs.co>
2022-08-19 20:25:37 +0530

    cosmostation wallet integration.

--- src/containers/NavBar/index.js
+++ src/containers/NavBar/index.js
@@ -7,9 +7,9 @@ import ExpansionButton from './ExpansionButton';
 import * as PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ClassNames from 'classnames';
-import { hideSideBar } from '../../actions/navBar';
+import { hideSideBar, showConnectDialog } from '../../actions/navBar';
 import Icon from '../../components/Icon';
-import { initializeChain } from '../../helper';
+import { initializeChain, initializeCosmoStation } from '../../helper';
 import { decode, encode } from 'js-base64';
 import { config } from '../../config';
 import { showMessage } from '../../actions/snackbar';
@@ -29,28 +29,38 @@ import {
     getValidators,
 } from '../../actions/stake';
 import { withRouter } from 'react-router-dom';
-import ConnectButton from './ConnectButton';
 import CopyButton from '../../components/CopyButton/TextButton';
 import variables from '../../utils/variables';
 import { fetchProposalDetails, fetchProposalTally, fetchVoteDetails, getProposals } from '../../actions/proposals';
+import { Button } from '@material-ui/core';
+import ConnectDialog from './ConnectDialog';
 
 class NavBar extends Component {
     constructor (props) {
         super(props);
+
+        this.state = {
+            cosmostationEvent: null,
+        };
 
         this.initKeplr = this.initKeplr.bind(this);
         this.handleFetch = this.handleFetch.bind(this);
         this.handleChain = this.handleChain.bind(this);
         this.getValidatorImage = this.getValidatorImage.bind(this);
         this.getProposalDetails = this.getProposalDetails.bind(this);
+        this.handleCosmoStation = this.handleCosmoStation.bind(this);
     }
 
     componentDidMount () {
-        if (localStorage.getItem('of_co_address')) {
+        if (localStorage.getItem('of_co_address') && (localStorage.getItem('of_co_wallet') === 'keplr')) {
             this.initKeplr();
+        } else if (localStorage.getItem('of_co_address') && (localStorage.getItem('of_co_wallet') === 'cosmostation')) {
+            this.handleCosmoStation(true);
         }
+
         if (this.props.proposals && !this.props.proposals.length &&
-            !this.props.proposalsInProgress && !this.props.stake) {
+            !this.props.proposalsInProgress && !this.props.stake &&
+            this.props.match && this.props.match.params && !this.props.match.params.proposalID) {
             this.props.getProposals((result) => {
                 if (result && result.length) {
                     const array = [];
@@ -73,6 +83,27 @@ class NavBar extends Component {
                     this.getProposalDetails(array && array.reverse());
                 }
             });
+        } else if (this.props.proposals && !this.props.proposalsInProgress && !this.props.stake &&
+            this.props.proposalDetails && Object.keys(this.props.proposalDetails).length === 1 &&
+            this.props.match && this.props.match.params && !this.props.match.params.proposalID) {
+            const array = [];
+            this.props.proposals.map((val) => {
+                const filter = this.props.proposalDetails && Object.keys(this.props.proposalDetails).length &&
+                    Object.keys(this.props.proposalDetails).find((key) => key === val.id);
+                if (!filter) {
+                    if (this.props.home && val.status !== 2) {
+                        return null;
+                    }
+
+                    array.push(val.id);
+                }
+                if (val.status === 2) {
+                    this.props.fetchProposalTally(val.id);
+                }
+
+                return null;
+            });
+            this.getProposalDetails(array && array.reverse());
         }
 
         if (this.props.address) {
@@ -93,11 +124,25 @@ class NavBar extends Component {
                 this.handleChain();
             }
         });
+
+        window.onload = () => {
+            if (window.cosmostation && window.cosmostation.cosmos) {
+                const cosmostationEvent = window.cosmostation.cosmos.on('accountChanged', () => {
+                    if (localStorage.getItem('of_co_address') || this.props.address !== '') {
+                        this.handleCosmoStation();
+                    }
+                });
+
+                this.setState({
+                    cosmostationEvent: cosmostationEvent,
+                });
+            }
+        };
     }
 
     componentDidUpdate (pp, ps, ss) {
         if ((!pp.proposals.length && (pp.proposals !== this.props.proposals) &&
-            this.props.proposals && this.props.proposals.length) ||
+                this.props.proposals && this.props.proposals.length) ||
             ((pp.address !== this.props.address) && (pp.address === '') && (this.props.address !== ''))) {
             this.props.proposals.map((val) => {
                 const votedOption = this.props.voteDetails && this.props.voteDetails.length && val && val.id &&
@@ -150,6 +195,10 @@ class NavBar extends Component {
 
     componentWillUnmount () {
         window.removeEventListener('keplr_keystorechange', this.handleChain);
+        if (this.state.cosmostationEvent) {
+            window && window.cosmostation && window.cosmostation.cosmos &&
+            window.cosmostation.cosmos.off(this.state.cosmostationEvent);
+        }
     }
 
     getValidatorImage (index, data) {
@@ -243,6 +292,28 @@ class NavBar extends Component {
         });
     }
 
+    handleCosmoStation (fetch) {
+        initializeCosmoStation((error, account) => {
+            if (error) {
+                this.props.showMessage(error);
+                localStorage.removeItem('of_co_address');
+
+                return;
+            }
+
+            const previousAddress = localStorage.getItem('of_co_address') &&
+                decode(localStorage.getItem('of_co_address'));
+            this.props.setAccountAddress(account && account.address);
+            if (fetch) {
+                this.handleFetch(account && account.address);
+            }
+            if (account && previousAddress !== account.address) {
+                localStorage.setItem('of_co_address', encode(account && account.address));
+                localStorage.setItem('of_co_wallet', 'cosmostation');
+            }
+        });
+    }
+
     render () {
         return (
             <div className={ClassNames('nav_bar padding', localStorage.getItem('of_co_address') || this.props.address
@@ -273,8 +344,13 @@ class NavBar extends Component {
                     </div>}
                     {localStorage.getItem('of_co_address') || this.props.address
                         ? <DisconnectButton/>
-                        : <ConnectButton proposalTab={this.props.proposalTab} stake={this.props.stake}/>}
+                        : <Button
+                            className="disconnect_button"
+                            onClick={() => this.props.showConnectDialog(this.props.proposalTab, this.props.stake)}>
+                            Connect
+                        </Button>}
                 </div>
+                <ConnectDialog/>
             </div>
         );
     }
@@ -306,6 +382,7 @@ NavBar.propTypes = {
     proposals: PropTypes.array.isRequired,
     setAccountAddress: PropTypes.func.isRequired,
     show: PropTypes.bool.isRequired,
+    showConnectDialog: PropTypes.func.isRequired,
     showDialog: PropTypes.func.isRequired,
     showMessage: PropTypes.func.isRequired,
     unBondingDelegationsInProgress: PropTypes.bool.isRequired,
@@ -329,6 +406,11 @@ NavBar.propTypes = {
         }),
     ),
     home: PropTypes.bool,
+    match: PropTypes.shape({
+        params: PropTypes.shape({
+            proposalID: PropTypes.string,
+        }),
+    }),
     proposalTab: PropTypes.bool,
     proposalsInProgress: PropTypes.bool,
     stake: PropTypes.bool,
@@ -387,6 +469,7 @@ const actionToProps = {
     fetchVoteDetails,
     fetchProposalTally,
     fetchProposalDetails,
+    showConnectDialog,
 };
 
 export default withRouter(connect(stateToProps, actionToProps)(NavBar));
